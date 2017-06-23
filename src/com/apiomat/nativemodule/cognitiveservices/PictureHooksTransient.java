15a786e781018be5c589b5faad11166351bc391e
Philipp Gillé <gille@apiomat.com>
2017-06-24 01:51:17 +0200

    Add basic logic for sending request to MS Cognitive Services
    - No saving of the result as Detection object yet

--- src/com/apiomat/nativemodule/cognitiveservices/PictureHooksTransient.java
+++ src/com/apiomat/nativemodule/cognitiveservices/PictureHooksTransient.java
@@ -1,15 +1,14 @@
-/*
- * Copyright (c) 2011 - 2017, Apinauten GmbH
+/* Copyright (c) 2011 - 2017, Apinauten GmbH
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
  * are permitted provided that the following conditions are met:
  *
- *  * Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *  * Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
+ * * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
@@ -20,102 +19,167 @@
  * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
- * OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+ * OF THE POSSIBILITY OF SUCH DAMAGE. */
 package com.apiomat.nativemodule.cognitiveservices;
 
-import java.util.*;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
-import com.apiomat.nativemodule.*;
-import com.apiomat.nativemodule.basics.User;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
+import com.apiomat.nativemodule.AbstractClientDataModel;
+import com.apiomat.nativemodule.IModelHooksTransient;
+import com.apiomat.nativemodule.Request;
 
 /**
-* Generated class for hooks on your Picture data model
-*/
+ * Generated class for hooks on your Picture data model
+ */
 
 public class PictureHooksTransient<T extends Picture> implements IModelHooksTransient<Picture>
 {
-    protected Picture model;
+	protected Picture model;
 
-    @Override
-    public void setCallingModel( Picture model )
-    {
-        this.model = model;
-    }
+	@Override
+	public void setCallingModel( Picture model )
+	{
+		this.model = model;
+	}
 
+	/* do-Methods can be used if your data model is set to TRANSIENT */
 
+	@Override
+	public String doPost( Picture obj, Request r )
+	{
+		return String.valueOf( System.currentTimeMillis( ) );
+	}
 
-    /*
-     * do-Methods can be used if your data model is set to TRANSIENT
-     */
+	@Override
+	public void doPut( Picture obj, Request r )
+	{
+		String appName = r.getApplicationName( );
+		CognitiveServices.AOM.log( appName, "foreign ID: " + obj.getForeignId( ), false );
 
-    @Override
-    public String doPost( Picture obj, Request r )
-    {
-        return null;
-    }
+		String picUrl = "http://poc.apiomat.enterprises" + obj.getContentURL( ) + ".img?apiKey=" + r.getApiKey( );
+		CognitiveServices.AOM.log( appName, "pic url: " + picUrl, false );
 
-    @Override
-    public void doPut( Picture obj, Request r )
-    {
-    }
+		String subKey =
+			( String ) CognitiveServices.APP_CONFIG_PROXY.getConfigValue( CognitiveServices.SUBSCRIPTION_KEY, appName,
+				r.getSystem( ) );
+		if ( subKey == null || "".equals( subKey ) )
+		{
+			CognitiveServices.AOM.throwException( appName, "No subscription key found in the module configuration" );
+		}
+		String jsonResult = faceRequest( subKey, picUrl );
+		CognitiveServices.AOM.log( appName, "jsonResult: " + jsonResult, false );
+	}
 
-    @Override
-    public Picture doGet( String foreignId, Request r )
-    {
-        return null;
-    }
+	private static String faceRequest( String subscriptionKey, String picUrl )
+	{
+		String result = null;
+		HttpClient httpclient = new DefaultHttpClient( );
 
-    @Override
-    public boolean doDelete( String foreignId, Request r )
-    {
-        return false;
-    }
-    
-    @Override
-    public boolean doDeleteAll( String query, Request r )
-    {
-        return false;
-    }
+		try
+		{
+			URIBuilder builder = new URIBuilder( "https://westeurope.api.cognitive.microsoft.com/face/v1.0/detect" );
 
-    @Override
-    public List<Picture> doGetAll( String query, Request r )
-    {
-        return null;
-    }
+			// Request parameters. All of them are optional.
+			builder.setParameter( "returnFaceId", "true" );
+			builder.setParameter( "returnFaceLandmarks", "false" );
+			builder.setParameter( "returnFaceAttributes",
+				"age,gender,headPose,smile,facialHair,glasses,emotion,hair,makeup,occlusion,accessories,blur,exposure,noise" );
 
-    @Override
-    public long doCountAll( String query, Request r )
-    {
-        return 0;
-    }
+			// Prepare the URI for the REST API call.
+			URI uri = builder.build( );
+			HttpPost request = new HttpPost( uri );
 
-    /*
-     * Please note: Before doPostRef gets called, doGet gets called internally,
-     * so that this.model can be populated with attribute values.
-     */
-    @Override
-    public void doPostRef( Object referencedObject, String referenceName, Request r )
-    {
-    }
+			// Request headers.
+			request.setHeader( "Content-Type", "application/json" );
+			request.setHeader( "Ocp-Apim-Subscription-Key", subscriptionKey );
 
-    /*
-     * Please note: Before doDeleteRef gets called, doGet gets called internally,
-     * so that this.model can be populated with attribute values.
-     */
-    @Override
-    public void doDeleteRef( String refName, String refForeignId, Request r )
-    {
-    }
+			// Request body.
+			StringEntity reqEntity = new StringEntity(
+				"{\"url\":\"" + picUrl + "\"}" );
+			request.setEntity( reqEntity );
 
-    /*
-     * Please note: Before doGetRef gets called, doGet gets called internally,
-     * so that this.model can be populated with attribute values.
-     */
-    @Override
-    public <Z extends AbstractClientDataModel> List<Z> doGetRef( String refName, String query, Request r )
-    {
-        return null;
-    }
+			// Execute the REST API call and get the response entity.
+			HttpResponse response = httpclient.execute( request );
+			HttpEntity entity = response.getEntity( );
+
+			if ( entity != null )
+			{
+				// Format and display the JSON response.
+				System.out.println( "REST Response:\n" );
+
+				result = EntityUtils.toString( entity ).trim( );
+			}
+		}
+		catch ( Exception e )
+		{
+			// Display error message.
+			System.out.println( e.getMessage( ) );
+		}
+		return result;
+	}
+
+	@Override
+	public Picture doGet( String foreignId, Request r )
+	{
+		return null;
+	}
+
+	@Override
+	public boolean doDelete( String foreignId, Request r )
+	{
+		return false;
+	}
+
+	@Override
+	public boolean doDeleteAll( String query, Request r )
+	{
+		return false;
+	}
+
+	@Override
+	public List<Picture> doGetAll( String query, Request r )
+	{
+		List<Picture> pictures = new ArrayList<>( );
+		Picture pic = new Picture( );
+		pic.setForeignId( String.valueOf( System.currentTimeMillis( ) ) );
+		pictures.add( pic );
+		return pictures;
+	}
+
+	@Override
+	public long doCountAll( String query, Request r )
+	{
+		return 0;
+	}
+
+	/* Please note: Before doPostRef gets called, doGet gets called internally,
+	 * so that this.model can be populated with attribute values. */
+	@Override
+	public void doPostRef( Object referencedObject, String referenceName, Request r )
+	{}
+
+	/* Please note: Before doDeleteRef gets called, doGet gets called internally,
+	 * so that this.model can be populated with attribute values. */
+	@Override
+	public void doDeleteRef( String refName, String refForeignId, Request r )
+	{}
+
+	/* Please note: Before doGetRef gets called, doGet gets called internally,
+	 * so that this.model can be populated with attribute values. */
+	@Override
+	public <Z extends AbstractClientDataModel> List<Z> doGetRef( String refName, String query, Request r )
+	{
+		return null;
+	}
 }
